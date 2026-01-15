0ad18aa8462c0bb64f6873871155bb94a1b90544
Cristian <cristian.alexa15@gmail.com>
2026-01-15 23:37:15 +0200

    uncom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# def main():
-#     print("Hello from repl-nix-workspace!")
-#
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    print("Hello from repl-nix-workspace!")
+
+
+if __name__ == "__main__":
+    main()
